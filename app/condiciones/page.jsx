f4fefabcc864053949d93f8d7df08ebf506e394f
andyukra <andyguachin87@gmail.com>
2026-09-03 14:35:49 -0300

    add politics and conditions routes

---
+++ app/condiciones/page.jsx
@@ -0,0 +1,9 @@
+
+export default function Condiciones() {
+  return (
+	<main>
+		<h1>Política de Privacidad</h1>
+		<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt cum eaque similique veniam assumenda dolore praesentium sapiente odio ipsum cupiditate ducimus doloremque itaque, aliquid quae officiis, sit voluptatum. Quibusdam, aspernatur sint quam aliquid, quasi accusantium exercitationem quae cum odit, fuga ea? Dolor quia temporibus at recusandae corporis tenetur cum quae.</p>
+	</main>
+  )
+}
